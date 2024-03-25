e8c910aa286a4c34ff11ecb883407e95e98d4d2e
onikd08 <onikd08@gmail.com>
2024-03-26 01:54:13 +0200

    Add subscribe and footer sections

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
 import AllProducts from "@/components/AllProducts";
 import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
+import Subscribe from "@/components/Subscribe";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <AllProducts />
+      <main>
+        <AllProducts />
+        <Subscribe />
+        <Footer />
+      </main>
     </>
   );
 }
